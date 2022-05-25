448d92effb8976cca48766a9f07d06094e67039e
dahee <dlekgml91@gmail.com>
2022-05-25 12:12:33 +0900

    no message

---
+++ todo-app/src/App.js
@@ -0,0 +1,63 @@
+import logo from './logo.svg';
+import { useState, useRef, useCallback } from 'react';
+import './App.css';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+
+const App=()=>{
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      text:'react의 기초 알아보기',
+      checked:true,
+    },
+    {
+      id:2,
+      text:'컴포넌트 스타일링해보기',
+      checked:true,
+    },
+    {
+      id:3,
+      text:'일정관리앱만들어보기',
+      checked:false,
+    },
+  ]);
+const nextId = useRef(4);
+
+const onInsert = useCallback(text=>{
+  const todo={
+    id:nextId.current,
+    text,
+    checked:false,
+  };
+  setTodos(todos.concat(todo));
+  nextId.current +=1;
+},
+[todos],
+);
+
+const onRemove = useCallback(
+  id=>{
+    setTodos(todos.filter(todo=>todo.id!==id));
+  },
+  [todos],
+);
+
+const onToggle = useCallback(
+  id => {
+    setTodos(
+      todos.map(todo=>
+        todo.id===id?{...todo, checked:!todo.checked}:todo,),
+    );
+  }, [todos],
+);
+
+  return (<TodoTemplate>
+    <TodoInsert onInsert={onInsert}></TodoInsert>
+    <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+  </TodoTemplate>
+  );
+};
+
+export default App;
